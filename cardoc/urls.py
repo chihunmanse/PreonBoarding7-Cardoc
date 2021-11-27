1eb7ef32d74f42fda959feee0d681de02cbe09b8
chihunmanse <chihunmanse@gmail.com>
2021-11-27 23:24:19 +0900

    [ADD] projectModeling

--- cardoc/urls.py
+++ cardoc/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-
+    # path("users", include("users.urls")),
+    # path("trims", include("trims.urls")),
 ]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id       = models.CharField(max_length = 50, unique = True, primary_key = True)
+    password = models.CharField(max_length = 200)
+    trims    = models.ManyToManyField('trims.Trim', through = 'UserTrim')
+
+    class Meta:
+        db_table = 'users'
+
+class UserTrim(models.Model):
+    user = models.ForeignKey('User', on_delete = models.CASCADE)
+    trim = models.ForeignKey('trims.Trim', on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'users_trims'
